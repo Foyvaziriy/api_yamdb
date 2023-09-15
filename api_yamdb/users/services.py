5d6c6e26b848174386e1c700daeddfabc952998d
Эмилар Локтев <emilar-l@yandex.ru>
2023-09-15 19:16:57 +0300

    Confirmation code obtain working

--- api_yamdb/users/services.py
+++ api_yamdb/users/services.py
@@ -1,33 +1,38 @@
 import string
 import random
-from django.core.mail import send_mail
 
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth import get_user_model
+from django.core.mail import send_mail
 
 
 User = get_user_model()
 
 
-def generate_confirmation_code(length=6):
+def generate_confirmation_code(length: int = 6) -> str:
     characters = string.ascii_letters + string.digits
     code = ''.join(random.choice(characters) for _ in range(length))
     return code
 
 
-def send_verification_email(to_email, verification_code, user_email=None):
-    subject = 'Код подтверждения регистрации'
-    message = f'Ваш код подтверждения: {verification_code}'
-    recipient_list = [user_email]
+def send_code(confirmation_code: str, user_email: str) -> None:
+    subject: str = 'Код подтверждения регистрации'
+    message: str = f'Ваш код подтверждения: {confirmation_code}'
+    from_email: str = 'production@yandex.ru'
+    recipient_list: list = [user_email]
 
-    send_mail(subject, message, recipient_list)
+    send_mail(
+        subject=subject,
+        message=message,
+        from_email=from_email,
+        recipient_list=recipient_list
+    )
 
-    print(f'Письмо сохранено в локальной папке для {to_email}')
+    print(f'Письмо сохранено в локальной папке для {user_email}')
 
 
 def get_tokens_for_user(user: User) -> dict[str, str]:
     refresh = RefreshToken.for_user(user)
-
     return {
         'access': str(refresh.access_token),
     }
