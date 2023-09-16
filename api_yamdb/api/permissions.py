67df5a5843dbe9f9bdfd6a2fa16859efcb3b0534
Эмилар Локтев <emilar-l@yandex.ru>
2023-09-16 13:59:43 +0300

    Small corrections

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -18,8 +18,8 @@ class IsAdminOrReadOnly(permissions.BasePermission):
 class AuthorAdminModeratorOrReadOnly(permissions.BasePermission):
     def has_permission(self, request: HttpRequest, view: ModelViewSet) -> bool:
         return (
-                request.method in permissions.SAFE_METHODS
-                or request.user.is_authenticated
+            request.method in permissions.SAFE_METHODS
+            or request.user.is_authenticated
         )
 
     def has_object_permission(self, request, view, obj):
