601b2b79aa5fe93ccbd80f192dc31d206117f5a4
Эмилар Локтев <emilar-l@yandex.ru>
2023-09-17 14:36:29 +0300

    Registration patch

--- api_yamdb/users/serializers.py
+++ api_yamdb/users/serializers.py
@@ -5,11 +5,22 @@ from rest_framework import serializers
 User = get_user_model()
 
 
+class ConfCodeField(serializers.SlugField):
+    def to_internal_value(self, data):
+        if isinstance(data, bool) or not isinstance(data, (str,)):
+            self.fail('invalid')
+        value = str(data)
+        return value.strip() if self.trim_whitespace else value
+
+
 class SignUpSerializer(serializers.Serializer):
     username = serializers.SlugField(max_length=150)
     email = serializers.EmailField(max_length=254)
 
+    def validate_username(self, value):
+        return not value == 'me'
+
 
 class AuthSerializer(serializers.Serializer):
     username = serializers.SlugField(max_length=150)
-    confirmation_code = serializers.CharField(max_length=256)
+    confirmation_code = ConfCodeField(max_length=256)
